7bb4cc7302e7f6cc22ffbfc3deb87b59b509f69a
xiaodong.lxd <382708101@qq.com>
2023-12-18 14:53:25 +0800

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,41 +12,126 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'English is not hard to study!', home: MyHomePage());
+    return ChangeNotifierProvider(
+        create: (context) {
+          DrawerItem.init();
+          return DrawerNotifier();
+        },
+        child: MaterialApp(
+            title: 'EnglishER',
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+            ),
+            home: MyHomePage()));
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var drawerState = context.watch<DrawerNotifier>();
+    return Scaffold(
+      appBar: AppBar(title: Text("Go")),
+      drawer: HomeDrawer(),
+      body: Center(
+        child: Text("${drawerState.getCurrentItem().name}"),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Material(
-      // Column is a vertical linear layout
-      child: Column(
-        children: [MyAppBar(title: Text('Follow me')), Expanded(child: Center(child: Text('Hello')))],
-      ),
-    );
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class DrawerNotifier extends ChangeNotifier {
+  var currentIndex = 0;
+
+  void onItemTapped(int index) {
+    currentIndex = index;
+    notifyListeners();
+  }
+
+  DrawerItem getCurrentItem() {
+    DrawerItem? item = DrawerItem.getItem(currentIndex);
+    if (item == null) {
+      return DrawerItem(-2, 'NULL');
+    } else {
+      return item;
+    }
+  }
+
+  int getCurrentIndex() {
+    return currentIndex;
   }
 }
 
+class DrawerItem {
+  int? _index;
+  String? _name;
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
+  DrawerItem(this._index, this._name);
 
-  final Widget title;
+  String? get name => _name;
+  int? get index => _index;
 
+  static final List<DrawerItem> _items = [];
+
+  static void init() {
+    _items.add(DrawerItem(0, 'Header'));
+    for (var i = 1; i < 10; i++) {
+      _items.add(DrawerItem(i, 'Item $i'));
+    }
+  }
+
+  static int getCount() {
+    return _items.length;
+  }
+
+  static DrawerItem? getItem(int index) {
+    return _items[index];
+  }
+}
+
+class _HomneDrawerState extends State<HomeDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      //Row is a horizontal linear layout
-      child: Row(
-        children: [const IconButton(onPressed: null, icon: Icon(Icons.menu)), Expanded(child: title)],
-      ),
-    );
+    var drawerState = context.watch<DrawerNotifier>();
+    return Drawer(
+        backgroundColor: Colors.orange,
+        child: ListView.builder(
+          itemCount: DrawerItem.getCount(),
+          itemBuilder: (context, index) {
+            debugPrint("build ListView index = $index");
+            DrawerItem? item = DrawerItem.getItem(index);
+            var name = item == null ? '' : item.name;
+            if (index == 0) {
+              return const DrawerHeader(
+                child: Text("Header"),
+                decoration: BoxDecoration(color: Colors.blue),
+              );
+            } else {
+              return ListTile(
+                title: Text('$name'),
+                selected: drawerState.getCurrentIndex() == index,
+                onTap: () {
+                  drawerState.onItemTapped(index);
+                  Navigator.pop(context);
+                },
+              );
+            }
+          },
+        ));
   }
 }
 
+class HomeDrawer extends StatefulWidget {
+  HomeDrawer({super.key});
+
+  @override
+  State<HomeDrawer> createState() => _HomneDrawerState();
+}
